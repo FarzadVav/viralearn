e6c93206661de03f129e5ea8ca755f6502b68734
FarzadVav <farzad.vav.work@gmail.com>
2025-10-19 00:43:38 -0700

    add responsive styles to Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,10 +12,10 @@ function Footer() {
   const t = useTranslations("App");
 
   return (
-    <footer className="glassy-card pt-20 pb-10 mt-40">
-      <div className="container grid grid-cols-3 gap-3 pb-10 border-b separator-border">
-        <div className="col-span-2">
-          <div className="f-align gap-3">
+    <footer className="glassy-card pt-20 pb-12 mt-40">
+      <div className="container grid grid-cols-1 lg:grid-cols-3 gap-6 pb-12 border-b">
+        <div className="lg:col-span-2">
+          <div className="f-align max-lg:justify-center gap-3">
             <Image
               alt={t("ProductName")}
               height={32}
@@ -25,29 +25,29 @@ function Footer() {
             <span className="font-morabba-medium">{t("ProductName")}</span>
           </div>
 
-          <p className="mt-3">
+          <p className="mt-3 max-lg:text-center">
             در آروین ویرا هر آنچه برای آنلاین کردن کسب و کارتان نیاز دارید برای شما مهیا
             کرده ایم. ما خدمات و محصولاتی داریم که تمام مراحل برای ورود کسب و کار شما به
             دنیای دیجیتال و توسعه آن را پوشش می دهند.
           </p>
 
-          <div className="f-align gap-3 mt-3">
+          <div className="f-align gap-3 mt-3 max-lg:justify-center">
             <div className="size-9 rounded-full bg-background"></div>
             <div className="size-9 rounded-full bg-background"></div>
             <div className="size-9 rounded-full bg-background"></div>
           </div>
         </div>
 
-        <div className="f-align justify-end gap-3">
+        <div className="f-align justify-center lg:justify-end gap-3">
           <div className="size-20 rounded-2xl bg-background"></div>
           <div className="size-20 rounded-2xl bg-background"></div>
         </div>
       </div>
 
-      <div className="container grid grid-cols-5 gap-3 mt-10 pb-10 border-b separator-border">
+      <div className="container grid grid-cols-2 lg:grid-cols-5 gap-6 mt-12 pb-12 border-b">
         <div>
-          <span className="font-yekan-bakh-bold opacity-50">امکانات</span>
-          <ul className="mt-3 space-y-1.5">
+          <span className="title">امکانات</span>
+          <ul className="mt-1.5 space-y-1.5">
             <li>مدیریت دوره آموزشی</li>
             <li>مدیریت وبینار</li>
             <li>مدیریت ارتباط با مشتریان </li>
@@ -55,8 +55,8 @@ function Footer() {
         </div>
 
         <div>
-          <span className="font-yekan-bakh-bold opacity-50">امکانات</span>
-          <ul className="mt-3 space-y-1.5">
+          <span className="title">امکانات</span>
+          <ul className="mt-1.5 space-y-1.5">
             <li>مدیریت دوره آموزشی</li>
             <li>مدیریت وبینار</li>
             <li>مدیریت ارتباط با مشتریان </li>
@@ -64,8 +64,8 @@ function Footer() {
         </div>
 
         <div>
-          <span className="font-yekan-bakh-bold opacity-50">امکانات</span>
-          <ul className="mt-3 space-y-1.5">
+          <span className="title">امکانات</span>
+          <ul className="mt-1.5 space-y-1.5">
             <li>مدیریت دوره آموزشی</li>
             <li>مدیریت وبینار</li>
             <li>مدیریت ارتباط با مشتریان </li>
@@ -73,8 +73,8 @@ function Footer() {
         </div>
 
         <div>
-          <span className="font-yekan-bakh-bold opacity-50">امکانات</span>
-          <ul className="mt-3 space-y-1.5">
+          <span className="title">امکانات</span>
+          <ul className="mt-1.5 space-y-1.5">
             <li>مدیریت دوره آموزشی</li>
             <li>مدیریت وبینار</li>
             <li>مدیریت ارتباط با مشتریان </li>
@@ -82,8 +82,8 @@ function Footer() {
         </div>
 
         <div>
-          <span className="font-yekan-bakh-bold opacity-50">امکانات</span>
-          <ul className="mt-3 space-y-1.5">
+          <span className="title">امکانات</span>
+          <ul className="mt-1.5 space-y-1.5">
             <li>مدیریت دوره آموزشی</li>
             <li>مدیریت وبینار</li>
             <li>مدیریت ارتباط با مشتریان </li>
@@ -91,9 +91,9 @@ function Footer() {
         </div>
       </div>
 
-      <div className="container grid grid-cols-3 mt-10">
-        <p className="col-span-2">© Design & Develop By ArvinVira Group</p>
-        <div className="f-align gap-3">
+      <div className="container grid grid-cols-1 lg:grid-cols-3 gap-6 mt-12">
+        <p className="lg:col-span-2 max-lg:text-center">© Design & Develop By ArvinVira Group</p>
+        <div className="f-align gap-3 max-lg:row-start-1">
           <Button>ارسال</Button>
           <InputGroup dir="ltr">
             <InputGroupAddon>
