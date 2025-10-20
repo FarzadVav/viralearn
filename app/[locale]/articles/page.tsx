690e6bbcdfec0b270bd33e9065085e3880cc8196
FarzadVav <farzad.vav.work@gmail.com>
2025-10-20 13:44:29 +0330

    add responsive styles to about-us page

--- app/[locale]/articles/page.tsx
+++ app/[locale]/articles/page.tsx
@@ -7,31 +7,35 @@ import { Button } from "@/components/ui/button";
 function ArticlesPage() {
   return (
     <>
-      <div className="container h-96 glassy-card rounded-2xl mt-6"></div>
-
-      <div className="w-max max-w-full f-align gap-1.5 p-1.5 mx-auto mt-40 rounded-full bordered-glassy-card [&>button]:rounded-full">
-        <Button>همه مقالات</Button>
-        <Button variant={"ghost"}>آموزش مجازی هاور</Button>
-        <Button variant={"ghost"}>آموزش هیبریدی</Button>
-        <Button variant={"ghost"}>ارزشبابی توصیفی</Button>
-        <Button variant={"ghost"}>مدیریت آموزش</Button>
-        <Button variant={"ghost"}>آموزش نوجوانان</Button>
+      <div className="container mt-6">
+        <div className="h-96 glassy-card rounded-2xl"></div>
       </div>
-      <div className="w-max max-w-full f-align gap-3 mx-auto mt-6">
-        <Input placeholder="جستجو..." />
-        <Button size={"icon"}>
-          <SearchIcon />
-        </Button>
+
+      <div className="container">
+        <div className="w-full lg:w-max lg:max-w-full f-align gap-1.5 p-1.5 mx-auto mt-40 rounded-full bordered-glassy-card [&>button]:rounded-full overflow-x-auto">
+          <Button>همه مقالات</Button>
+          <Button variant={"ghost"}>آموزش مجازی هاور</Button>
+          <Button variant={"ghost"}>آموزش هیبریدی</Button>
+          <Button variant={"ghost"}>ارزشبابی توصیفی</Button>
+          <Button variant={"ghost"}>مدیریت آموزش</Button>
+          <Button variant={"ghost"}>آموزش نوجوانان</Button>
+        </div>
+        <div className="w-full lg:w-max lg:max-w-full f-align gap-3 mx-auto mt-6">
+          <Input placeholder="جستجو..." />
+          <Button size={"icon"}>
+            <SearchIcon />
+          </Button>
+        </div>
       </div>
 
       <div className="container mt-40">
-        <h1 className="text-5xl font-morabba-bold">محبوب ترین مقالات</h1>
-        <p className="m-3">
+        <h1 className="heading max-lg:text-center">محبوب ترین مقالات</h1>
+        <p className="m-3 max-lg:text-center">
           در این بخش پربازدیدترین و مفیدترین مطالب درباره ERP و مدیریت سازمانی را مشاهده
           کنید. این مقالات توسط کاربران مورد توجه قرار گرفته و برای بهبود عملکرد کسب‌وکار
           شما توصیه می‌شوند.
         </p>
-        <div className="grid mt-6 grid-cols-3 gap-6">
+        <div className="grid mt-6 grid-cols-1 lg:grid-cols-3 gap-3">
           {Array.from({ length: 6 }).map((_, i) => (
             <div
               key={i}
@@ -39,7 +43,7 @@ function ArticlesPage() {
               <div className="w-full aspect-video bg-background rounded-2xl" />
               <Link
                 href={`/articles/${i}`}
-                className="font-yekan-bakh-bold line-clamp-2 hover:underline">
+                className="title line-clamp-2 hover:underline">
                 ۲۰ راه حل ساده برای کاهش هزینه های سازمان
               </Link>
               <p className="leading-relaxed line-clamp-3">
